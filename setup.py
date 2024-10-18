ead3b948812b8b5a6036adf62c44b9a5db950ad9
Nathan Xavier <45743862+nathanxavier@users.noreply.github.com>
2024-10-18 08:13:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,16 +21,16 @@ setup(
     url='https://github.com/nathanxavier/CVSegGuide',
     python_requires='>=3.8',
     ext_modules=[
-    CUDAExtension(
-        'adaptive_conv_cuda_impl',
-        [
-            'models/backbones/featup/adaptive_conv_cuda/adaptive_conv_cuda.cpp',
-            'models/backbones/featup/adaptive_conv_cuda/adaptive_conv_kernel.cu',
-        ]),
-    CppExtension(
-        'adaptive_conv_cpp_impl',
-        ['models/backbones/featup/adaptive_conv_cuda/adaptive_conv.cpp'],
-        undef_macros=["NDEBUG"]),
+        CUDAExtension(
+            'adaptive_conv_cuda_impl',
+            [
+                'featup/adaptive_conv_cuda/adaptive_conv_cuda.cpp',
+                'featup/adaptive_conv_cuda/adaptive_conv_kernel.cu',
+            ]),
+        CppExtension(
+            'adaptive_conv_cpp_impl',
+            ['featup/adaptive_conv_cuda/adaptive_conv.cpp'],
+            undef_macros=["NDEBUG"]),
     ],
     cmdclass={
         'build_ext': BuildExtension
