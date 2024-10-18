def022718be3c5ea8ba7d7c21df07b6cb554f7ec
Nathan Xavier <45743862+nathanxavier@users.noreply.github.com>
2024-10-18 07:54:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 
 setup(
     name='CVSegGuide',
@@ -19,4 +20,16 @@ setup(
     ],
     url='https://github.com/nathanxavier/CVSegGuide',
     python_requires='>=3.8',
+        ext_modules=[
+    CUDAExtension(
+        'adaptive_conv_cuda_impl',
+        [
+            'featup/adaptive_conv_cuda/adaptive_conv_cuda.cpp',
+            'featup/adaptive_conv_cuda/adaptive_conv_kernel.cu',
+        ]),
+    CppExtension(
+        'adaptive_conv_cpp_impl',
+        ['featup/adaptive_conv_cuda/adaptive_conv.cpp'],
+        undef_macros=["NDEBUG"]),
+    ],
 )
